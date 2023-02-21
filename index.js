2ae799721c91898c681099035f148edc29e126c2
Schuyler Alschuler <salschuler@amplify.com>
2023-02-21 14:50:50 -0800

    Checkpoint

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ $ = jQuery;
 
 inputs = $(".student-sandbox-navigation-container .dcg-mq-editable-field");
 
-testInputs = ["12", "dog", "5"];
+// testInputs = ["12", "dog", "5"];
+testInputs = [
+  "1",
+  "-1",
+  "0",
+  "\\frac{1}{2}",
+  "100",
+  "-100",
+  "10000",
+  "10000",
+  "10^{100000}",
+  "-10^{100000}",
+  "x=1",
+  "x=-1",
+  "x=0",
+  "x=\\frac{1}{2}",
+  "x=100",
+  "x=-100",
+  "1=x",
+  "-1=x",
+  "0=x",
+  "\\frac{1}{2}=x",
+  "100=x",
+  "-100=x",
+];
 
 output = [];
 
@@ -23,37 +47,45 @@ async function loop() {
       );
       mq = Desmos.MathQuill(inputs[i]);
 
-      found = inputs.eq(i).parents().eq(7).find(".mq-error-message");
-      console.log(inputs[i]);
-      console.log(found);
-      if (found.length > 0) {
-        warning = mq.getAriaPostLabel();
-        outStr = `Math Input [${
-          i + 1
-        }] has warning on input ${mq.latex()}:\n\t${warning}`;
-        console.log(outStr);
-        output.push(outStr);
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        continue;
-      }
-
-      found = $(".student-sandbox-navigation-container .dcg-mq-editable-field")
+      foundInput = inputs.eq(i).parents().eq(7).find(".mq-error-message");
+      foundTable = $(
+        ".student-sandbox-navigation-container .dcg-mq-editable-field"
+      )
         .eq(i)
         .parents()
         .eq(7)
         .find(".table-error-message");
-      if (found.length > 0) {
-        outStr = `Table Input [${
+      // console.log(inputs[i]);
+      // console.log(foundInput);
+
+      if (foundInput.length > 0) {
+        warning = mq.getAriaPostLabel();
+        outStr = `Math Input [${
           i + 1
-        }] has warning on input ${mq.latex()}:\n\tCan't currently display table warnings.`;
-        console.log(outStr);
+        }] has warning on input ${mq.latex()}:\n\t${warning}`;
+        // console.log(outStr);
         output.push(outStr);
         await new Promise((resolve) => setTimeout(resolve, 100));
         continue;
+      } else if (foundTable.length > 0) {
+        outStr = `Table Input [${
+          i + 1
+        }] has warning on input ${mq.latex()}:\n\tCan't currently display table warnings.`;
+        // console.log(outStr);
+        output.push(outStr);
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        continue;
+      } else {
+        outStr = `Input [${
+          i + 1
+        }] has NO warning on input ${mq.latex()}\n\tN/A.`;
+        // console.log(outStr);
+        output.push(outStr);
       }
     }
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
 }
 
+console.log("Final output:");
 console.log(output.join("\n"));
